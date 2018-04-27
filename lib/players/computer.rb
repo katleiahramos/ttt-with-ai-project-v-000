df3e55d3da403eacf3506817f4bbf55067768267
Katleiah Ramos <katleiahramos@gmail.com>
2018-04-27 17:16:26 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -9,7 +9,7 @@ module Players
 
       board.cells.each_with_index {|cell, index|
         binding.pry
-        open << index if cell == " "
+        cell == " "? open << index : nil
       }
 
       best_move = open.select {|open_i| open_i == 4 || open_i ==  0 || open_i == 2 || open_i == 6 || open_i == 8}.first
