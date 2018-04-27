8c0c65ff88e8f9701981d38625f4e74b001edcbd
Katleiah Ramos <katleiahramos@gmail.com>
2018-04-27 17:07:56 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -13,13 +13,14 @@ module Players
 
       best_move = open.select {|open_i| open_i == 4 || open_i ==  0 || open_i == 2 || open_i == 6 || open_i == 8}.first
 
-      #first two turns 
+      #first two turns
       if best_move
       #returns position 5 if open
       open.include?(4) ? "5" : "#{best_move + 1}" #takes best move index and converts to position string
       else
         binding.pry
-        open.first  
+        open.first
+      end
 
     end
 
